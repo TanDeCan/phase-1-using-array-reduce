87fee2391bc670bc98e50d7c2ceb3d3062c4e265
VictoriaBladow <vbladow9999@yahoo.com>
2022-11-17 17:02:45 -0600

    commit tree 611fbd3d7a579fe47046e0659d7b32dd06918f78

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+//function addBatteries(batteryBatches) {
+    //let totalBatteries = 0;
+
+    //for(const batteries of batteryBatches) {
+   //     totalBatteries += batteries;
+   // };
+
+    //return totalBatteries
+//}
+
+//console.log(addBatteries(batteryBatches))
+
+
+const startingBatteries = 0;
+const totalBatteries = batteryBatches.reduce((noBatteries, allBatteries) => noBatteries + allBatteries, startingBatteries);
+
+console.log(totalBatteries)
